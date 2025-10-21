f39f784263033ca9a28e54089828fac75826344e
SAMIKA BHAVESH BHATKAR <samikabhatkar@gmail.com>
2025-10-20 19:47:55 -0700

    New mine

--- main.js
+++ main.js
@@ -7,16 +7,26 @@ class Block{
     this.data = data; 
     this.previousHash = previousHash;
     this.hash = this.calculateHash(); 
+    this.nonce = 0; 
     } 
 
     calculateHash() { 
     return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
     }  
+
+    mineBlock(difficulty) {
+      while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {  
+        this.nonce++;
+        this.hash = this.calculateHash(); 
+
   }
-      
+  
+  
+
 class Blockchain{ 
     constructor() {
     this.chain = [this.createGenesisBlock()]; 
+    this.difficulty = 2; 
 
     }
     
@@ -30,7 +40,7 @@ class Blockchain{
 
     addBlock(newBlock) { 
     newBlock.previousHash = this.getLatestBlock().hash; 
-    newBlock.hash = newBlock.calculateHash(); 
+    newBlock.mineBlock(this.difficulty); 
     this.chain.push(newBlock); 
     }
 
@@ -50,15 +60,11 @@ class Blockchain{
     }
 }
 let samikaCoin = new Blockchain();
+
+console.log('Mining block 1...'); 
 samikaCoin.addBlock(new Block(1, "10/07/2023", { amount: 4 }));
+
+console.log('Mining block 2...'); 
 samikaCoin.addBlock(new Block(2, "12/07/2023", { amount: 10 }));
 
-console.log('Is blockchain valid? ' + samikaCoin.isChainValid());
 
-console.log(JSON.stringify(samikaCoin, null, 4));
-
-// Tampering with the blockchain
-samikaCoin.chain[1].data = { amount: 100 };
-samikaCoin.chain[1].hash = samikaCoin.chain[1].calculateHash();
-
-console.log('Is blockchain valid after tampering? ' + samikaCoin.isChainValid());
